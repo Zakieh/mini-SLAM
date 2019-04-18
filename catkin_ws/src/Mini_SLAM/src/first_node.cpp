24f950642d19e3cdd9fc7948264545e7b7c1bfce
zakieh <zakiehsa@buffalo.edu>
2019-04-17 20:16:16 -0400

    making the program modular and creating the robot_world class

--- catkin_ws/src/Mini_SLAM/src/first_node.cpp
+++ catkin_ws/src/Mini_SLAM/src/first_node.cpp
@@ -3,18 +3,56 @@
 #include "sensor_msgs/PointCloud2.h"
 
 
+class robot_world
+{
+private:
+
+	int world_map[10][10];
+	int robot_x;
+	int robot_y;
+	int robot_orientation; 
+
+	ros::NodeHandle nh;	
+
+public:
+
+	robot_world()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				if (i > 0 && i < 9 && j > 0 && j < 9)
+					world_map[i][j] = 0;
+				else
+					world_map[i][j] = 1;
+			}
+		}
+		world_map[0][6] = 0;
+		world_map[3][3] = 1;
+		world_map[3][6] = 1;
+		world_map[4][0] = 0;
+		world_map[9][0] = 0;
+		world_map[5][0] = 0;
+		world_map[6][3] = 1;
+		world_map[6][6] = 1;
+		world_map[9][7] = 0;
+		world_map[9][8] = 0;
+
+		//initializing robot first pose
+		robot_x = 9;
+		robot_y = 7;
+		robot_orientation = 0;
+	}
+    
+    void update_robot_pose();
+
+   
+};
+
 int main(int argc, char **argv)
 {
-	int world_map[10][10] = {
-							 {1, 1, 1, 1, 1, 1, 0, 1, 1, 1},
-							 {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-							 {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-							 {1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
-							 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-							 {0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-							 {1, 0, 0, 1, 0, 0, 1, 0, 0, 1},
-							 {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-							 {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-							 {1, 1, 1, 1, 1, 1, 1, 0, 0, 1},
-							};
+	ros::init(argc, argv, "first_node");
+	
+	robot_world my_world;
 }
