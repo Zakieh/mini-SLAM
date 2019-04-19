2a7b575de75e4e82c9b1ed2f575870b35571bab4
zakieh <zakiehsa@buffalo.edu>
2019-04-19 13:08:54 -0400

    changes my assumption about local map for scan matching, local map is equal to previous scan

--- catkin_ws/src/Mini_SLAM/src/second_node.cpp
+++ catkin_ws/src/Mini_SLAM/src/second_node.cpp
@@ -19,6 +19,7 @@ private:
 	std::pair<int, int> robot_orientation;
 
 	int current_sensor_reading[3][5];
+	int previous_sensor_reading[3][5]; // as local map for scan matcher
 
 	bool first_map_fill; 
 
@@ -148,7 +149,7 @@ void mini_SLAM::scanCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 			} 
 		}
 	}
-	std::cout << "=================current readin =============" << std::endl;
+	std::cout << "=================current reading =============" << std::endl;
 	for (int i = 0 ; i < 3; i++)
 	{
 		for (int j = 0 ; j < 5; j++)
@@ -185,106 +186,59 @@ void mini_SLAM::scanCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 		}*/
 	}
 
-	//std::cout << "pcl size " << pcl_cloud.points.size() << std::endl;
-
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0 ; j < 5 ; j++)
+		{
+			previous_sensor_reading[i][j] = current_sensor_reading[i][j];
+		}
+	}
 }
 void mini_SLAM::scan_matcher()
 {
 	// for scan matching we only check one cell forward, and 90 degree rotation left and 90 degree rotation right
-	//first we should extract the local map
+	// for local map we only assume the previous scan
 
-	int** local_map = new int*[5];
-	for (int i = 0 ; i < 5 ; i++)
-		local_map[i] = new int[5];
-
-	for (int i = 0; i < 5 ; i++)
-	{
-		for (int j = 0; j < 5 ; j++)
-			local_map[i][j] = -1;
-	}
-	local_map[2][2] = 0; //robot_pose
-   	
-   	int localmap_min_x = std::max(robot_x - 2, 0);
-	int localmap_min_y = std::max(robot_y - 2, 0);
-	int localmap_max_x = std::min(robot_x + 2, 9);
-	int localmap_max_y = std::min(robot_y + 2, 9);
-
-	for (int i = localmap_min_x ; i <= localmap_max_x ; i++)
-	{
-		for (int j = localmap_min_y; j <= localmap_max_y; j++)
-		{
-			local_map[i - localmap_min_x][j - localmap_min_y] = world_map[i][j];
-		}
-	}
-
-	std::cout << "================== local map =============" << std::endl;
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-			std::cout << local_map[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-
-	if (robot_orientation.second == -1) //we should turn the local map 90 degree clock wise for simpler scan matching
-	{
-		turn_local_map(local_map);
-	}
-	if (robot_orientation.first == 1) //we should turn the local map 180 degree clock wise
-	{
-		for (int i=0; i < 2 ; i++)
-			turn_local_map(local_map);
-	}
-	if (robot_orientation.second == 1) //we should turn the local map 270 degree clock wise
-	{
-		for (int i=0; i < 3 ; i++)
-			turn_local_map(local_map);
-	}
-		
-	//check moving forward: in this case, the top two rows of local map should be compared with the bottom two rows of current sensor reading
+	//check moving forward: in this case, the top two rows of previous_sensor_reading should be compared with the bottom two rows of current sensor reading
 	int match_forward = 0;
 	for (int i = 1 ; i < 3; i++)
 	{
 		for (int j = 0 ; j < 5; j++)
 		{
-			if (current_sensor_reading[i][j] != -1 && local_map[i-1][j] != -1)
+			if (current_sensor_reading[i][j] != -1 && previous_sensor_reading[i-1][j] != -1)
 			{
-				(current_sensor_reading[i][j] - local_map[i-1][j]) == 0 ? match_forward++ : match_forward--;
+				(current_sensor_reading[i][j] - previous_sensor_reading[i-1][j]) == 0 ? match_forward++ : match_forward--;
 			}
 		}
 	}
 
-	// turn left 90 degree
+	// turn left 90 degree, the left 3*3 of previous sensor reading compared to right 3*3 of current sensor reading
 
 	int match_turn_left = 0;
 	for (int i = 0 ; i < 3; i++)
 	{
-		for (int j = 0 ; j < 5; j++)
+		for (int j = 2 ; j < 5; j++)
 		{
-			if (current_sensor_reading[i][j] != -1 && local_map[4-j][i] != -1)
+			if (current_sensor_reading[i][j] != -1 && previous_sensor_reading[4-j][i] != -1)
 			{
-				(current_sensor_reading[i][j] - local_map[4-j][i]) == 0 ? match_turn_left++ : match_turn_left--;
+				(current_sensor_reading[i][j] - previous_sensor_reading[4-j][i]) == 0 ? match_turn_left++ : match_turn_left--;
 			}
 		}
 	}
 
-	// turn right 90 degree
+	// turn right 90 degree, the right 3*3 of previous sensor reading compared to left 3*3 of current sensor reading
 
 	int match_turn_right = 0;
 	for (int i = 0 ; i < 3; i++)
 	{
-		for (int j = 0 ; j < 5; j++)
+		for (int j = 0 ; j < 3; j++)
 		{
-			if (current_sensor_reading[i][j] != -1 && local_map[j][4-i] != -1)
+			if (current_sensor_reading[i][j] != -1 && previous_sensor_reading[j][4-i] != -1)
 			{
-				(current_sensor_reading[i][j] - local_map[j][4-i]) == 0 ? match_turn_right++ : match_turn_right--;
+				(current_sensor_reading[i][j] - previous_sensor_reading[j][4-i]) == 0 ? match_turn_right++ : match_turn_right--;
 			}
 		}
 	}
-
-	std::cout << "costs " << match_forward << " " << match_turn_left << " " << match_turn_right << std::endl;
-
 
 	if (match_forward > match_turn_right && match_forward > match_turn_left)
 	{
